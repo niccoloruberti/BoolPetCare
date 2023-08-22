2eac27907a262c13788181d06abb93c2b11c2340
AntonelloStefania <antonelloste94@gmail.com>
2023-08-22 18:20:19 +0200

    [add] index

---
+++ resources/views/admin/animals/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Specie</th>
+                    <th scope="col">Owner</th>
+                    <th scope="col">Size</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($animals as $animal)
+                    <tr>
+                        <th>{{$animal->name}}</th>
+                        <td>{{$animal->specie}}</td>
+                        <td>{{$animal->owner}}</td>
+                        <td>{{$animal->size}}</td>
+
+                        {{-- SHOW BUTTON --}}
+                        <td>
+                            <a href="{{route('admin.animals.show', $animal->id)}}" class="btn btn-sm btn-success">
+                                <i class="fas fa-eye"></i>
+                            </a>
+                        </td>
+
+                        {{-- EDIT BUTTON --}}
+                        <td>
+                            <a href="#" class="btn btn-sm btn-warning">
+                                <i class="fas fa-edit"></i>
+                            </a>
+                        </td>
+
+                        {{-- DELETE BUTTON --}}
+                        <td>
+                            <form class="d-inline-block" action="#" onsubmit="return confirm('Sei sicuro di voler eliminare questo progetto?')" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm btn-danger">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+          </table>
+    </div>
+</div>
+@endsection
