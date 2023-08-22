91e0ee0f1b4ee95fdeaa9dce4e28ae7e40d7078c
niccoloruberti <nikiruberti@gmail.com>
2023-08-22 16:57:16 +0200

    creazione route

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Animal extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'specie', 'date_of_birth', 'genre', 'owner', 'weight', 'size', 'img_link'];
 }
