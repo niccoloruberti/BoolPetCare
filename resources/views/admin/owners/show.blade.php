870191d34ea3d24f8e102cd7bdb7b699a5c59cad
niccoloruberti <nikiruberti@gmail.com>
2023-09-02 23:34:28 +0200

    Aggiunta lista animali all'interno della show di un proprietario

--- resources/views/admin/owners/show.blade.php
+++ resources/views/admin/owners/show.blade.php
@@ -25,6 +25,56 @@
                     </div>
                 </div>
             </div>
+
+            {{-- Mostra tutti gli animali del proprietario --}}
+            <div class="col-12 mt-4">
+                <h4>Animals:</h4>
+                <ul>
+                    @foreach ($owner->animals as $animal)
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Specie</th>
+                                <th scope="col">Size</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>{{$animal->name}}</th>
+                                <td>{{$animal->specie}}</td>
+                                <td>{{$animal->size}}</td>
+        
+                                {{-- SHOW BUTTON --}}
+                                <td>
+                                    <a href="{{route('admin.animals.show', $animal->id)}}" class="btn btn-sm btn-success">
+                                        <i class="fas fa-eye"></i>
+                                    </a>
+                                </td>
+        
+                                {{-- EDIT BUTTON --}}
+                                <td>
+                                    <a href="{{ route('admin.animals.edit', $animal->id)}}" class="btn btn-sm btn-warning">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                </td>
+        
+                                {{-- DELETE BUTTON --}}
+                                <td>
+                                    <form class="d-inline-block" action="{{ route('admin.animals.destroy', $animal->id)}}" onsubmit="return confirm('Sei sicuro di voler eliminare questo animale?')" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-sm btn-danger">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>                            
+                        </tbody>
+                    </table>
+                    @endforeach
+                </ul>
+            </div>
         </div>
     </div>
 @endsection
