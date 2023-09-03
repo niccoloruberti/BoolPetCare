523f9bc0ed541602c42b36ca776b99a4a1ff7dc3
AntonelloStefania <antonelloste94@gmail.com>
2023-09-03 22:58:46 +0200

    iniziato stile show.animals

--- resources/views/admin/animals/show.blade.php
+++ resources/views/admin/animals/show.blade.php
@@ -15,25 +15,30 @@
                 {{-- MODIFICATA ROUTE PER PORTARCI DIETRO L'ID DELL'ANIMALE, PROBABILMENTE DA FARE ANCHE NEL PASSAGGIO DATI A STORE PERCHE' RESTITUISCE NULL --}}
             <a href="{{route('admin.animals.index.desease', ['animal' => $animal])}}" class="btn btn-sm btn-primary">Cartella Clinica</a>
         </div>
-            <div class="col-12">
-                <div class="d-flex justify-content-between align-items-center">
+            <div class="col-12 text-center fw-bold my-5">
+                <h2>More Info about <span style="color:#E04F00">{{$animal->name}}</span> The <span style="color:#E04F00">{{$animal->specie}}</span></h2>
+            </div>
+            <div class="col-12 d-flex">
+
+                <div class="d-flex justify-content-around col-7 align-items-center">
                     <div>
-                        
-                        <h3>Animal's Name: {{$animal->name}}</h3>
-                        <h3>Owner: {{($animal->owner) ? $animal->owner->name.' '.$animal->owner->surname : ''}}</h3>
-                        <h4>Specie: {{$animal->specie}}</h4>
+                        <h3> Animal's Name: <span style="color:#E04F00">{{$animal->name}}</span></h3>
+                        <h3>Owner: <span style="color:#E04F00">{{($animal->owner) ? $animal->owner->name.' '.$animal->owner->surname : ''}}</span></h3>
+                        <h4>Specie:<span style="color:#E04F00"> {{$animal->specie}}</span></h4>
+                    </div>
+                    <div>
                         <h6>Date of birth: {{$animal->date_of_birth}}</h6>
                         <h6>Genre: {{$animal->genre}}</h6>
                         <h6>Weight: {{$animal->weight}} kg</h6>
                         <h6>Size: {{$animal->size}}</h6>
-
                     </div>
                 </div>
-                <div>
-                    <img class="my-5" src="{{$animal->img_link}}" alt="">
+                <div class="col-5 d-flex justify-content-end">
+                    <img class="my-5" src="{{$animal->img_link}}" alt="" width="300px">
                 </div>
-               
             </div>
+               
+            
         </div>
     </div>
 @endsection
