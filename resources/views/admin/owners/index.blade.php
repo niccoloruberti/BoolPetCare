5da3415b9eb2b9bbb0aa5a0641a822a5ef3a980e
AntonelloStefania <antonelloste94@gmail.com>
2023-09-04 12:38:16 +0200

    stile owner

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -2,21 +2,31 @@
 
 @section('content')
 <div class="container">
+    <div class="col-12">
+        <a href="{{route('admin.dashboard')}}" class="btn btn-md back-button"><i class="fa-regular fa-circle-left fa-l me-2" style="color: #ad4e1a;"></i>Dashboard</a>
+    </div>
     <div class="row">
+        
         <div class="col-12 my-5 d-flex justify-content-end">
             {{-- <form action="{{ route('admin.animals.search') }}" method="GET">
                 <input type="text" name="search" required/>
                 <button class="btn btn-success ms-3" type="submit">Search</button>
             </form> --}}
-            <a class="btn btn-warning me-2" href="{{ route('admin.owners.create')}}">Add owner</a>
+            <a class="btn add-button" href="{{ route('admin.owners.create')}}"><i class="fa-solid fa-circle-plus me-2" style="color: #ffffff;"></i> Add owner</a>
         </div>
-        <table class="table table-striped">
+        <div class="mb-4">
+            <h2>Registered <span style="color: #ad4e1a">Owners <i class="fas fa-person "></i></span></h2>
+        </div>
+        <table class="table table-hover">
             <thead>
                 <tr>
                     <th scope="col">Name</th>
-                    <th scope="col">Cognome</th>
-                    <th scope="col">mail</th>
-                    <th scope="col">telefono</th>
+                    <th scope="col">Surname</th>
+                    <th scope="col">e-mail</th>
+                    <th scope="col">Telephone number</th>
+                    <th scope="col">See more</th>
+                    <th scope="col">Modify</th>
+                    <th scope="col">Delete</th>
                 </tr>
             </thead>
             <tbody>
@@ -30,14 +40,14 @@
 
                         {{-- SHOW BUTTON --}}
                         <td>
-                            <a href="{{route('admin.owners.show', $owner->id)}}" class="btn btn-sm btn-success">
+                            <a href="{{route('admin.owners.show', $owner->id)}}" class="btn btn-sm show-button">
                                 <i class="fas fa-eye"></i>
                             </a>
                         </td>
 
                         {{-- EDIT BUTTON --}}
                         <td>
-                            <a href="{{ route('admin.owners.edit', $owner->id)}}" class="btn btn-sm btn-warning">
+                            <a href="{{ route('admin.owners.edit', $owner->id)}}" class="btn btn-sm edit-button">
                                 <i class="fas fa-edit"></i>
                             </a>
                         </td>
@@ -47,7 +57,7 @@
                             <form class="d-inline-block" action="{{ route('admin.owners.destroy', $owner->id)}}" onsubmit="return confirm('Sei sicuro di voler eliminare questo proprietario?')" method="POST">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-sm btn-danger">
+                                <button type="submit" class="btn btn-sm delete-button">
                                     <i class="fas fa-trash"></i>
                                 </button>
                             </form>
