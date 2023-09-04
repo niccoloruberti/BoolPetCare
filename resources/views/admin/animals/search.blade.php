dcef2cf2a9b9c41f81a9ab96c1594ae26e9df5a9
AntonelloStefania <antonelloste94@gmail.com>
2023-09-04 13:57:37 +0200

    [fix] search view

--- resources/views/admin/animals/search.blade.php
+++ resources/views/admin/animals/search.blade.php
@@ -3,22 +3,27 @@
 @section('content')
 @if($animals->isNotEmpty())
 <div class="container">
+    <div class="my-4 d-flex justify-content-start">
+        <a href="{{route('admin.animals.index')}}" class="btn btn-sm back-button"><i class="fa-regular fa-circle-left fa-l me-2" style="color: #ad4e1a;"></i>All Animals</a>
+    </div>
+   
     <div class="row">
-        <div class="col-12 my-5 text-end">
-            <a href="{{ route('admin.animals.index')}}">
-                <button class="btn btn-primary btn-sm">All Animals</button>
-            </a>
-        </div>
+       <div class="text-center my-4">
+        <h2>Search Results</h2>
+       </div>
     </div>
     <div class="row">
         <div class="col-12">
-            <table class="table table-striped">
+            <table class="table table-hover">
                 <thead>
                     <tr>
                         <th scope="col">Name</th>
                         <th scope="col">Specie</th>
                         <th scope="col">Owner</th>
                         <th scope="col">Size</th>
+                        <th scope="col">See More</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -27,19 +32,19 @@
                         <tr>
                             <th>{{$animal->name}}</th>
                             <td>{{$animal->specie}}</td>
-                            <td>{{$animal->owner}}</td>
+                            <td>{{$animal->owner->$name}}</td>
                             <td>{{$animal->size}}</td>
             
                             {{-- SHOW BUTTON --}}
                             <td>
-                                <a href="{{route('admin.animals.show', $animal->id)}}" class="btn btn-sm btn-success">
+                                <a href="{{route('admin.animals.show', $animal->id)}}" class="btn btn-sm show-button">
                                     <i class="fas fa-eye"></i>
                                 </a>
                             </td>
             
                             {{-- EDIT BUTTON --}}
                             <td>
-                                <a href="{{route('admin.animals.edit', $animal->id)}}" class="btn btn-sm btn-warning">
+                                <a href="{{route('admin.animals.edit', $animal->id)}}" class="btn btn-sm edit-button">
                                     <i class="fas fa-edit"></i>
                                 </a>
                             </td>
@@ -49,7 +54,7 @@
                                 <form class="d-inline-block" action="{{route('admin.animals.destroy', $animal->id)}}" onsubmit="return confirm('Sei sicuro di voler eliminare questo animale?')" method="POST">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="btn btn-sm btn-danger">
+                                    <button type="submit" class="btn btn-sm delete-button">
                                         <i class="fas fa-trash"></i>
                                     </button>
                                 </form>
