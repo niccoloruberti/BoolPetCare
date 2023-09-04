f22c6b64f834d53e4a27a31ebbe6622d6c63b543
AntonelloStefania <antonelloste94@gmail.com>
2023-09-04 12:51:07 +0200

    stile create owner

--- resources/views/admin/owners/create.blade.php
+++ resources/views/admin/owners/create.blade.php
@@ -2,46 +2,46 @@
 
 @section('content')
 <div class="container">
+    <div class="my-4 d-flex justify-content-start">
+        <a href="{{ route('admin.owners.index') }}" class="btn btn-sm back-button"><i class="fa-regular fa-circle-left fa-l me-2" style="color: #ad4e1a;"></i> All Owners</a>
+    </div>
    <div class="row">
-       <div class="col-12 my-5">
-           <h1 class="">Add new owner</h1>
-           <div class="col-12 text-end">
-              <div class="">
-                  <a href="{{ route('admin.owners.index') }}" class="btn btn-sm btn-primary">All Owners</a>
-              </div>
-           </div>
-       </div>
+    <div class="col-12 my-5 text-center">
+        <h1 class="">Add New <span style="color:#E04F00">Owner</span></h1>
+    </div>
         
        <div class="col-12">
           <form action="{{ route('admin.owners.store') }}" method="POST">
             @csrf
-            {{-- nome --}}
-            <div class="form-group mt-4">
-               <label for="name">Name</label>
-               <input type="text" name="name" id="name" class="form-control"  value="{{ old('name') }}">
-            </div>
+            <div class="form-group mt-4 d-flex justify-content-around">
+                <div class="d-flex align-items-center">
+                    <label for="name" class="fw-bold me-3">Name: </label>
+                    <input type="text" name="name" id="name" class="form-control"  required>
+                </div>
+            
             {{-- cognome --}}
-            <div class="form-group mt-4">
-                <label for="name">Surname</label>
-                <input type="text" name="surname" id="surname" class="form-control"  value="{{ old('surname') }}">
+                <div class="d-flex align-items-center">
+                    <label for="name" class="fw-bold me-3">Surname: </label>
+                    <input type="text" name="surname" id="surname" class="form-control"  required>
+                </div>
             </div>
             {{-- indirizzo --}}
             <div class="form-group mt-4">
-                <label for="name">Address</label>
-                <input type="text" name="address" id="address" class="form-control"  value="{{ old('address') }}">
+                <label for="name" class="fw-bold mb-2">Address:</label>
+                <input type="text" name="address" id="address" class="form-control"  required>
             </div>
             {{-- telefono --}}
             <div class="form-group mt-4">
-                <label for="name">Phone</label>
-                <input type="text" name="phone" id="phone" class="form-control"  value="{{ old('phone') }}">
+                <label for="name" class="fw-bold mb-2">Phone:</label>
+                <input type="text" name="phone" id="phone" class="form-control"   required>
             </div>
             {{-- email --}}
             <div class="form-group mt-4">
-                <label for="name">Email</label>
-                <input type="mail" name="email" id="email" class="form-control"  value="{{ old('email') }}">
+                <label for="name" class="fw-bold mb-2">Email:</label>
+                <input type="mail" name="email" id="email" class="form-control"   required>
              </div>
             <div class="form-group my-4 col-12 text-center">
-                <button type="submit" class="btn btn-success">Save</button>
+                <button type="submit" class="btn edit-button">Save</button>
             </div>
         </form>
        </div>
